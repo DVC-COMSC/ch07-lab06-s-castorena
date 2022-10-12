a751422c1272b6bb653b029dd60ccbcb216d629b
sarah castorena <ceanz@yahoo.com>
2022-10-11 22:39:47 -0700

    new commit

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-
-inputvalues = input('Enter all elements values: ')
+# Sarah castorena
+# SID 
+inputvalues = input('Enter all elements: ')
 numbers1 = inputvalues.split() 
 for i in range(len(numbers1)):
 	numbers1[i] = int(numbers1[i]) 
+numbers2 = []
+for i in range(len(numbers1)):
+    x = numbers1.pop()
+    numbers2.append(x)
+print(numbers1)
+print(numbers2)
+
 # The following line is the same as the for-loop
 # numbers1 = list(map(int, numbers))
 
